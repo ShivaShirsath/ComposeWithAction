740e046272ff24a049d602743ab201b6d4ed20c4
Shiva Shirsath <59221352+ShivaShirsath@users.noreply.github.com>
2022-01-17 01:21:07 +0530

    Update Color.kt

--- app/src/main/java/ss/composewithaction/ui/theme/Color.kt
+++ app/src/main/java/ss/composewithaction/ui/theme/Color.kt
@@ -2,13 +2,7 @@ package ss.composewithaction.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
-
-val T200 = Color(0x22222222)
-val T500 = Color(0x55555555)
-val T700 = Color(0x77777777)
-val Tl200 = Color(0x11221122)
-
+val Orange200 = Color(0xFFFFCC00)
+val Orange500 = Color(0xFFFF8800)
+val Orange700 = Color(0xFFFF4400)
+val Green200  = Color(0xFF66CC00)
